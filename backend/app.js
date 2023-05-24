4c32486a4e42f695af39e5cd942c17b8a1208a08
Zeyad Abdullah <zeyadAbdullah@gmail.com>
2023-05-24 06:09:03 +0300

    update project

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
+import mongoose from "mongoose";
+import cors from "cors";
 
 // middleware
 const corsOptions = {
@@ -13,7 +15,9 @@ app.use(cors(corsOptions));
 
 // connect MongoDB
 mongoose
-  .connect("mongodb+srv://pizzaTeam:test1234@cluster0.6e9bp37.mongodb.net/?retryWrites=true&w=majority")
+  .connect(
+    "mongodb+srv://pizzaTeam:test1234@cluster0.6e9bp37.mongodb.net/?retryWrites=true&w=majority"
+  )
   .then(() => {
     const PORT = 4000 || 8000;
     app.listen(PORT, () => {
@@ -24,7 +28,6 @@ mongoose
     console.log(err);
   });
 
-// route
-app.get("/", (req, res) => {
-  res.status(201).json({ message: "Connected to Backend!" });
-});
+// routes
+import routes from "./routes/index.js";
+routes(app);
